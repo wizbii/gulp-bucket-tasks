791375b83a6efe0b9d9d14ffa6f6c9a0f294e8eb
Gabin <hello@gabinaureche.com>
2016-03-25 22:00:34 +0100

    apply editorconfig style rules

--- scripts.js
+++ scripts.js
@@ -6,17 +6,17 @@ var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 
 module.exports = function (config, options) {
-    return [
-        function () {
-            return gulp
-                .src(config.files)
-                .pipe(sourcemaps.init())
-                .pipe(concat(config.output.file))
-                .pipe(babel({ presets: ['es2015'], compact: true, comments: false }))
-                .pipe(uglify())
-                .pipe(sourcemaps.write('./'))
-                .pipe(gulp.dest(config.output.path))
-            ;
-        }
-    ];
+  return [
+    function () {
+      return gulp
+        .src(config.files)
+        .pipe(sourcemaps.init())
+        .pipe(concat(config.output.file))
+        .pipe(babel({presets: ['es2015'], compact: true, comments: false}))
+        .pipe(uglify())
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest(config.output.path))
+      ;
+    }
+  ];
 };
